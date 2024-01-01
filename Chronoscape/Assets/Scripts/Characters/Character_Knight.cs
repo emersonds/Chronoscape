dfc116f3308412c659230d678ced6be34e43773b
Dylan Emerson <emersonds2868@gmail.com>
2024-01-01 09:52:59 -0500

    Added Comments for future me, tweaked dash forces
    - Lowered dash cooldown to 1 for now, for testing.
    - Tweaked moving dash force to 2 and standing dash force to 15 to feel even.
    - Added plenty of documentation code for when we have to look back at this in a month for our second class.

--- Chronoscape/Assets/Scripts/Characters/Character_Knight.cs
+++ Chronoscape/Assets/Scripts/Characters/Character_Knight.cs
@@ -25,11 +25,13 @@ public class Character_Knight : PlayerController
     // If the player is dashing
     private bool isDashing = false;
 
+
     // Start is called before the first frame update
     protected override void Start()
     {
         base.Start();
     }
+
 
     // Update is called once per frame
     protected override void Update()
@@ -37,16 +39,25 @@ public class Character_Knight : PlayerController
         base.Update();
     }
 
+
     protected override void FixedUpdate()
     {
         base.FixedUpdate();
     }
+
 
     protected override void GetInput()
     {
         base.GetInput();
     }
 
+
+    /// <summary>
+    /// Updates player velocity on a per-axis bases
+    /// </summary>
+    /// <param name="playerVelocity">The player's current velocity in a direction (x or z)</param>
+    /// <param name="moveInput">The player's current input direction (x or z)</param>
+    /// <returns>The velocity component (x or z)</returns>
     protected override float UpdateAxisVelocity(float playerVelocity, float moveInput)
     {
         // If there is input from the player
@@ -78,47 +89,77 @@ public class Character_Knight : PlayerController
         return playerVelocity;
     }
 
+
+    /// <summary>
+    /// Activates the Knight's dash roll.
+    /// </summary>
     protected override void ActivateBasicAbility()
     {
+        // Activate dash if it is not on cooldown
         if (canDash)
         {
+            // Save moveVec at instant the ability is cast
             Vector3 currMoveVec = moveVec;
+            // Start dash
             StartCoroutine(DashTimer(currMoveVec));
         }
     }
 
 
+    /// <summary>
+    /// Plays a rolling animation while speeding up or applying
+    /// a force to the Knight character, depending on if the player
+    /// is standing still or moving. Always rolls in the direction
+    /// the player is facing.
+    /// </summary>
+    /// <param name="dir">The player's move vector at time of ability cast.</param>
+    /// <returns></returns>
     private IEnumerator DashTimer(Vector3 dir)
     {
+        // Stop the player from dashing again
         canDash = false;
+        // Start the dash
         isDashing = true;
+        // Store current velocity
         Vector3 tempVelocity = rb.velocity;
 
+        // If player is moving, increase velocity by movingDashForce.
         if (dir != Vector3.zero)
         {
             animator.SetBool("isDashing", true);
             rb.velocity *= movingDashForce;
             yield return new WaitForSeconds(dashDuration);
         }
+        // If the player is not moving, apply an impulse force so the player doesn't dash in place.
         else
         {
             animator.SetBool("isDashing", true);
+            // Transform.forward is the direction the player is facing.
             rb.AddForce(transform.forward * standingDashForce, ForceMode.Impulse);
             yield return new WaitForSeconds(dashDuration);
         }
 
+        // Set velocity back to original velocity
         rb.velocity = tempVelocity;
+        // End dash and animation
         animator.SetBool("isDashing", false);
         isDashing = false;
+        // Trigger dash cooldown
         StartCoroutine(DashCooldown());
     }
 
+
+    /// <summary>
+    /// The dash ability cooldown; how long until the player can dash again.
+    /// </summary>
+    /// <returns></returns>
     private IEnumerator DashCooldown()
     {
+        // Wait for cooldown to end and allow the player to dash again.
         yield return new WaitForSeconds(dashCooldown);
         canDash = true;
     }
-        
+
 
     protected override void ActivateUltimateAbility()
     {
